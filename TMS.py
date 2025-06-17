172ddeab69451f429cfa7847811fa7b3337883c1
oalnasra <oa22313@my.westlake.school.nz>
2025-06-17 13:36:19 +1200

    Dictionaries completed

---
+++ TMS.py
@@ -0,0 +1,55 @@
+task_dictionary = {
+    "T1": {
+        "Title": "Design Homepage",
+        "Description": "Create a mockup of the homepage",
+        "Assignee": "JSM",
+        "Priority": 3,
+        "Status": "In Progress"
+    },
+    "T2": {
+        "Title": "Implement Login page",
+        "Description": "Create the Login page for the website",
+        "Assignee": "JSM",
+        "Priority": 3,
+        "Status": "Blocked"
+    },
+    "T3": {
+        "Title": "Fix navigation menu",
+        "Description": "Fix the navigation menu to be more user-friendly",
+        "Assignee": "None",
+        "Priority": 1,
+        "Status": "Not Started"
+    },
+    "T4": {
+        "Title": "Add payment processing",
+        "Description": "Implement payment processing for the website",
+        "Assignee": "JLO",
+        "Priority": 2,
+        "Status": "In Progress"
+    },
+    "T5": {
+        "Title": "Create an About Us page",
+        "Description": "Create a page with information about the company",
+        "Assignee": "BDI",
+        "Priority": 1,
+        "Status": "Blocked"
+    }
+}
+
+Team_members = {
+    "JSM": {
+        "Name": "John Smith",
+        "Email": "John@techvision.com",
+        "Tasks_Assigned": ["T1", "T2"]
+    },
+    "JLO": {
+        "Name": "Jane Love",
+        "Email": "Jane@techvision.com",
+        "Tasks_Assigned": ["T4"]
+    },
+    "BDI": {
+        "Name": "Bob Dillon",
+        "Email": "Bob@techvision.com",
+        "Tasks_Assigned": ["T5"]
+    }
+}
